b25b14e389614a542af0ff00dbae001f249dfa8d
fgchng <107479561+fgchng@users.noreply.github.com>
2022-12-06 14:11:02 +0800

    Add files via upload

---
+++ New folder/general.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Router to Home
+router.get("/", (req, res) => {
+    res.render("general/index");
+});
+
+module.exports = router;
