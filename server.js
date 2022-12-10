db69447a8b50d9e99b77ce1ade2c7a33cfaa661c
fgchng <107479561+fgchng@users.noreply.github.com>
2022-12-11 04:49:16 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname + 'public/css')));
+app.use(express.static(path.join(__dirname, 'public/css')));
 
 
 var isLoggedIn = false;
